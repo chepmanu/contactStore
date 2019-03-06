2053b58d70a94d6479704c9195cd4288cbcc5f88
chepmanu <emmanuelchepsire33@gmail.com>
2019-03-06 09:42:25 +0300

    change firebase configs

--- phone-book/firebase.js
+++ phone-book/firebase.js
@@ -1,9 +1,9 @@
 // Initialize Firebase
 module.exports = {
-    apiKey            : "AIzaSyD_xxvi-nhmj2JB5i3PD_Pe1GN3wai9T4w",
-    authDomain        : "phone-book-9b50d.firebaseapp.com",
-    databaseURL       : "https://phone-book-9b50d.firebaseio.com",
-    projectId         : "phone-book-9b50d",
-    storageBucket     : "phone-book-9b50d.appspot.com",
-    messagingSenderId : "563002322421"
+    apiKey            : "",
+    authDomain        : "",
+    databaseURL       : "",
+    projectId         : "",
+    storageBucket     : "",
+    messagingSenderId : ""
 };
--- phone-book/services/fireinit.js
+++ phone-book/services/fireinit.js
@@ -3,23 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/database';
 
-// if (!firebase.app.length){
-//     console.log('Hey bruhh');
-//     firebase.initializeApp({
-//         apiKey            : "AIzaSyD_xxvi-nhmj2JB5i3PD_Pe1GN3wai9T4w",
-//         authDomain        : "phone-book-9b50d.firebaseapp.com",
-//         databaseURL       : "https://phone-book-9b50d.firebaseio.com",
-//         projectId         : "phone-book-9b50d",
-//         storageBucket     : "phone-book-9b50d.appspot.com",
-//         messagingSenderId : "563002322421"
-//     });
 var config = {
-    apiKey            : "AIzaSyD_xxvi-nhmj2JB5i3PD_Pe1GN3wai9T4w",
-    authDomain        : "phone-book-9b50d.firebaseapp.com",
-    databaseURL       : "https://phone-book-9b50d.firebaseio.com",
-    projectId         : "phone-book-9b50d",
-    storageBucket     : "phone-book-9b50d.appspot.com",
-    messagingSenderId : "563002322421"
+    apiKey            : "",
+    authDomain        : "",
+    databaseURL       : "",
+    projectId         : "",
+    storageBucket     : "",
+    messagingSenderId : ""
 };
 
 !firebase.apps.length ? firebase.initializeApp(config) : '';
